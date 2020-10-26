c1b093f358bb94490e2aad8486d25b0edd0b1c8a
ghemsley <ghemsley@protonmail.ch>
2020-10-26 12:14:59 -0500

    commit tree 731ef59d01a1f5986851028d4b7c4eed37495f69

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest_key = nil
+  if name_hash != {}
+    name_hash.each do |key, value|
+      if lowest_key.nil?
+        lowest_key = key
+      elsif name_hash[lowest_key] > value
+        lowest_key = key
+      end
+    end
+  end
+  lowest_key
 end
